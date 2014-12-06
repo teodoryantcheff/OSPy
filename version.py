98579ff6de45264562eae0880938ebc962f671ca
Teodor Yantcheff <teodor.yantchev@gmail.com>
2014-12-06 23:20:28 +0200

    vesrions.py: small git related change

--- version.py
+++ version.py
@@ -11,17 +11,18 @@ import logging
 ##############################
 
 major_ver = 2
-minor_ver = 2
+minor_ver = 4
+ver_date = '2014-12-07'
 
 try:
     revision = int(subprocess.check_output(['git', 'rev-list', '--count', '--first-parent', 'HEAD']))
 except Exception:
-    logging.warning("Could not use git to determine revision!")
-    revision = 999
-ver_str = '%d.%d.%d' % (major_ver, minor_ver, revision)
+    logging.warning('Could not use git to determine revision!')
+    revision = 'no-git'
+ver_str = '{}.{}.{}'.format(major_ver, minor_ver, revision)
 
 try:
     ver_date = subprocess.check_output(['git', 'log', '-1', '--format=%cd', '--date=short']).strip()
 except Exception:
-    logging.warning("Could not use git to determine date of last commit!")
+    logging.warning('Could not use git to determine date of last commit!')
     ver_date = '2014-09-28'
